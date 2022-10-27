70799d2cf5f6582c86f9cd801829c69b94496e92
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-27 14:19:42 -0300

    Perfil de teste

--- src/main/java/io/helioanacronista/helpdesk/HelpdeskApplication.java
+++ src/main/java/io/helioanacronista/helpdesk/HelpdeskApplication.java
@@ -1,48 +1,14 @@
 package io.helioanacronista.helpdesk;
 
-import io.helioanacronista.helpdesk.domain.entities.Chamado;
-import io.helioanacronista.helpdesk.domain.entities.Cliente;
-import io.helioanacronista.helpdesk.domain.entities.Tecnico;
-import io.helioanacronista.helpdesk.domain.enums.Perfil;
-import io.helioanacronista.helpdesk.domain.enums.Prioridade;
-import io.helioanacronista.helpdesk.domain.enums.Status;
-import io.helioanacronista.helpdesk.repositories.ChamadoRepository;
-import io.helioanacronista.helpdesk.repositories.ClienteRepository;
-import io.helioanacronista.helpdesk.repositories.TecnicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private ChamadoRepository chamadoRepository;
+public class HelpdeskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Tecnico myTecnico = new Tecnico(null, "Valdir cezar", "63653230268", "valdir@mail.com", "123");
-		myTecnico.addPerfil(Perfil.ADMIN);
-
-		Cliente firstCliente = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", "123");
-
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", myTecnico, firstCliente);
-
-		tecnicoRepository.saveAll(Arrays.asList(myTecnico));
-		clienteRepository.saveAll(Arrays.asList(firstCliente));
-		chamadoRepository.saveAll(Arrays.asList(c1));
-	}
 }
---
+++ src/main/java/io/helioanacronista/helpdesk/config/TestConfig.java
@@ -0,0 +1,20 @@
+package io.helioanacronista.helpdesk.config;
+
+import io.helioanacronista.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("teste")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instanciaDB() {
+        this.dbService.instanciaDB();
+    }
+}
